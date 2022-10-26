285a3a82b79af039641c501ca3b19926a3c5f6f3
CGraciolli <cgraciolli@gmail.com>
2022-10-26 10:39:55 +0200

    mod

--- blend_matrix.py
+++ blend_matrix.py
@@ -1,4 +1,54 @@
-from typing import List
+from typing import List, Set, Tuple
+
+def average(nums:List[int]) -> int:
+    """
+    recives a non-empty list of integers,
+    returns the average
+    """
+    return sum(nums)/len(nums)
+
+def get_neighbors_indexes(matrix:List[List[int]], index_row:int, index_column:int) -> Set[Tuple[int]]:
+    """
+    recives a matrix with numerical values and two integers representing a position in the matrix
+    returns a set of the element's neighbors' positions (represented as tuples)
+    the element itself is also considered a neighbor
+    """
+    neighborhood = {(index_row, index_column)} ##the element itself
+    if index_column > 0:
+        neighborhood.add((index_row, index_column - 1)) ##neighbor to the left
+    if index_column < len(matrix[0]) - 1:
+        neighborhood.add((index_row, index_column + 1)) ##neighbor to the right
+    if index_row > 0:
+        neighborhood.add((index_row - 1, index_column)) ##neighbor above
+    if index_row < len(matrix) - 1:
+        neighborhood.add((index_row + 1, index_column)) ##neighbor below
+    return neighborhood
+
+
+def get_value_from_indexes(matrix:List[List[int]], index_row:int, index_column:int) -> int:
+    """
+    recives a matrix and two integers representing a position in the matrix
+    returns the element of the matrix occupying that position
+    """
+    return matrix[index_row][index_column]
+
+def get_neighbors(matrix:List[List[int]], index_row:int, index_column:int) -> List[int]:
+    """
+    recives a matrix with numerical values and two integers representing a position in the matrix
+    returns a list of the element's neighbors (the element is also included in the list)
+    """
+    neighborhood_indexes = get_neighbors_indexes(matrix, index_row, index_column)
+    neighborhood = list(map(lambda x: get_value_from_indexes(matrix, x[0], x[1]), neighborhood_indexes))
+    return neighborhood
+
+def process_element(matrix:List[List[int]], index_row:int, index_column:int) -> int:
+    """
+    recives a matrix with numerical values and two integers representing a position in the matrix
+    returns the average between that element and its neighbors
+    """
+    neighbors = get_neighbors(matrix, index_row, index_column)
+    print(neighbors)
+    return average(neighbors)
 
 def process_matrix(matrix:List[List[int]]) -> List[List[int]]:
     """
@@ -8,20 +58,10 @@ def process_matrix(matrix:List[List[int]]) -> List[List[int]]:
     the element of the same position in the old matrix and its neighbors
     """
     new_matrix = []
-    for row_index, row in enumerate(matrix):
+    for row_index in range(len(matrix)):
         new_row = []
-        for column_index, element in enumerate(row):
-            list_neighbors = [element] ##we put the element itself in the list of neighbors for simplicity's sake
-            if column_index > 0:
-                list_neighbors.append(row[column_index - 1]) ##neighbor to the left
-            if column_index < len(row) - 1:
-                list_neighbors.append(row[column_index + 1]) ##neighbor to the right
-            if row_index > 0:
-                list_neighbors.append(matrix[row_index - 1][column_index]) ##neighbor above
-            if row_index < len(matrix) - 1:
-                 list_neighbors.append(matrix[row_index + 1][column_index]) ##neighbor below
-            ##the value in the new matrix is the average between the element and it's neighbors
-            new_element = sum(list_neighbors)/len(list_neighbors)
+        for column_index in range(len(matrix[0])):
+            new_element = process_element(matrix, row_index, column_index)
             new_row.append(new_element)
         new_matrix.append(new_row)
     return new_matrix
--- blend_matrix_test.py
+++ blend_matrix_test.py
@@ -1,30 +1,50 @@
 import pytest
-from blend_matrix import process_matrix, process_matrix_dic
+from blend_matrix import process_matrix, average, get_neighbors_indexes, get_value_from_indexes, process_element
 
-def test_empty():
-    assert process_matrix([[]]) == [[]]
+def test_average():
+    l1 = [0, 0, 0]
+    l2 = [-4]
+    l3 = [0.5, -0.5, 3]
+
+    assert average(l1) == 0
+    assert average(l2) == -4
+    assert average(l3) == 1
+
+def test_get_neighbors_indexes():
+    m1 = [[3, 0, 1], [0, 3, 1]]
+    one_number = [[2]]
+
+    assert get_neighbors_indexes(m1, 0, 0) == {(0, 0), (0, 1), (1, 0)}
+    assert get_neighbors_indexes(m1, 1, 0) == {(0, 0), (1, 0), (1, 1)}
+    assert get_neighbors_indexes(m1, 0, 1) == {(0, 1), (0, 0), (0, 2), (1, 1)}
+    assert get_neighbors_indexes(one_number, 0, 0) == {(0, 0)}
+
+def test_get_value_from_indexes():
+    m1 = [[3, 0, 1], [0, 6, -9]]
+
+    assert get_value_from_indexes(m1, 0, 0) == 3
+    assert get_value_from_indexes(m1, 0, 2) == 1
+    assert get_value_from_indexes(m1, 1, 2) == -9
+
+def test_process_element():
+    m1 = [[3, 0], [0, 3]]
+
+    assert process_element(m1, 0, 0) == 1
 
 def test_process_matrix():
     m1 = [[3, 0], [0, 3]]
     zeroes = [[0, 0, 0], [0, 0, 0]]
+    m2 = [[1, 4], [-3, -9], [0, -13]]
     one_row = [[1, 2, 3]]
     one_column = [[1], [-1], [10]]
+    one_number = [[4]]
+    empty = [[]]
     
     assert process_matrix(m1) == [[1, 2], [2, 1]]
     assert process_matrix(zeroes) == zeroes
     assert process_matrix(one_row) == [[1.5, 2, 2.5]]
     assert process_matrix(one_column) == [[0], [10/3], [4.5]]
+    assert process_matrix(m2) == [[2/3, -4/3], [-11/4, -21/4], [-16/3, -22/3]]
+    assert process_matrix(one_number) == one_number
+    assert process_matrix(empty) == empty
 
-def test_empty_dic():
-    assert process_matrix_dic([[]]) == [[]]
-
-def test_process_matrix_dic():
-    m1 = [[3, 0], [0, 3]]
-    zeroes = [[0, 0, 0], [0, 0, 0]]
-    one_row = [[1, 2, 3]]
-    one_column = [[1], [-1], [10]]
-    
-    assert process_matrix_dic(m1) == [[1, 2], [2, 1]]
-    assert process_matrix_dic(zeroes) == zeroes
-    assert process_matrix_dic(one_row) == [[1.5, 2, 2.5]]
-    assert process_matrix_dic(one_column) == [[0], [10/3], [4.5]]
